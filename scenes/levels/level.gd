f1dc24e6a2d4eac15b6f683a025971def94e292c
Lu Xu <thusimon@gmail.com>
2023-10-29 20:31:47 -0700

    learn collision and timer

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
-var logo
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	logo = $Logo
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if logo.position.x > 1000:
-		logo.position.x = 0
+	pass
 
+
+
+func _on_entry_body_entered(body):
+	print('entered: ', body)
+
+
+func _on_entry_body_exited(body):
+	print('exit: ', body)
--- scenes/players/player.gd
+++ scenes/players/player.gd
@@ -1,18 +1,33 @@
-extends Node2D
+extends CharacterBody2D
 
 
 const speed = 200
+var can_laser = true
+var can_grenade = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(100,100) # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
-	position += direction * 100 * delta
+	velocity = direction * speed
+	move_and_slide()
 	
-	if Input.is_action_pressed("Primary"):
+	if Input.is_action_pressed("Primary") and can_laser:
+		can_laser = false;
 		print('shoot laser')
-	if Input.is_action_pressed("Secondary"):
+		$LaserTimer.start()
+	if Input.is_action_pressed("Secondary") and can_grenade:
+		can_grenade = false;
 		print('shoot grenade')
+		$GrenadeTimer.start()
+
+
+func _on_laser_timer_timeout():
+	can_laser = true
+
+
+func _on_grenade_timer_timeout():
+	can_grenade = true

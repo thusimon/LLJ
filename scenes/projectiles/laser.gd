755bcdcd6fa31ea095990cdd99291649c68935ac
Lu Xu <thusimon@gmail.com>
2023-11-12 13:43:11 -0800

    remove laser on hit

--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -4,9 +4,19 @@ extends Area2D
 var direction: Vector2 = Vector2.UP
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$SelfDestoryTimer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += direction * speed * delta
+
+
+func _on_body_entered(body):
+	if "hit" in body:
+		body.hit()
+	queue_free()
+
+
+func _on_self_destory_timer_timeout():
+	queue_free()

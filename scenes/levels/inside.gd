45ee80e454d4137f7dfd8c0bde8b186a9d9f0f50
Lu Xu <thusimon@gmail.com>
2023-11-20 17:47:21 -0800

    scene transit

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,6 +1,9 @@
 extends LevelBase
 
+#var inside_next_scene: PackedScene = preload("res://scenes/levels/outside.tscn")
 
 func _on_exit_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, 'speed', 0, 0.5)
+	#get_tree().change_scene_to_packed(inside_next_scene)
+	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -1,8 +1,13 @@
 extends LevelBase
 
+#var outside_next_scene: PackedScene = preload("res://scenes/levels/inside.tscn")
+
 func _on_gate_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, 'speed', 0, 0.5)
+	#get_tree().change_scene_to_packed(outside_next_scene)
+	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
+	
 
 func _on_gate_body_exited(_body):
 	pass

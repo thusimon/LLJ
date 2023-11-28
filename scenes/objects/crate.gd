04dc1d28a743afd0fa36a0c1e914abaddcbf2f8a
Lu Xu <thusimon@gmail.com>
2023-11-27 19:13:15 -0800

    update reward direction

--- scenes/objects/crate.gd
+++ scenes/objects/crate.gd
@@ -1,5 +1,7 @@
 extends ItemContainer
 
+@onready var init_direction: Vector2 = Vector2.DOWN.rotated(rotation)
+
 var is_hit = false
 
 func hit():
@@ -8,6 +10,5 @@ func hit():
 		$UpSprite.hide()
 		var pos = $SpawnPosition.global_position
 		for i in range(3):
-			var direction = Vector2(0, 1)
-			direction = direction.rotated(randf_range(0, 2*PI))
+			var direction = init_direction.rotated(randf_range(-PI/2, PI/2))
 			open.emit(pos, direction)

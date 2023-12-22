09ec51216b67df7066c78296f540a02d706b9e8b
Lu Xu <thusimon@gmail.com>
2023-12-22 14:18:17 -0800

    add hunter bone

--- scenes/players/hunter.gd
+++ scenes/players/hunter.gd
@@ -10,13 +10,18 @@ func _ready():
 func setup():
 	await get_tree().physics_frame
 	set_physics_process(true)
+	$NavigationAgent2D.path_desired_distance = 4.0
+	$NavigationAgent2D.target_desired_distance = 4.0
 	$NavigationAgent2D.target_position = Globals.player_pos
 	
 func _physics_process(_delta):
-	var next_path_pos: Vector2 = $NavigationAgent2D.get_next_path_position()
-	var direction: Vector2 = (next_path_pos - global_position).normalized()
-	velocity = direction * speed
-	move_and_slide()
+	if active:
+		var next_path_pos: Vector2 = $NavigationAgent2D.get_next_path_position()
+		var direction: Vector2 = (next_path_pos - global_position).normalized()
+		velocity = direction * speed
+		var tween = create_tween()
+		tween.tween_property(self, 'rotation', direction.angle() + PI / 2, 0.4)
+		move_and_slide()
 
 func _on_watch_area_body_entered(_body):
 	active = true
